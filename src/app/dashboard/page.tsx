d51cfc08e94b05313efdcf30a569f0591486069d
serkan <40214329+serayd61@users.noreply.github.com>
2025-12-03 14:48:26 +0100

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,6 @@
-import Link from 'next/link';
 'use client';
 
+import Link from 'next/link';
 import { useState, useEffect } from 'react';
 import { useSession, signOut } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
