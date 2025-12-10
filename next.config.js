ea6dbabebf2634fe9e238296451677f8640f60ca
serayd61 <serayd61@hotmail.com>
2025-12-10 22:19:35 +0100

    Fix TypeScript target and remove deprecated serverActions config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
 }
 
